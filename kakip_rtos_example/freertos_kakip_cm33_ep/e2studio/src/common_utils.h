846e9c70a9c9ce957bf18dcd91ca4178625d307f
Jason Yang <jason98166@gmail.com>
2026-04-29 15:00:56 +0800

    kakip_rtos_example: freertos_kakip_cm33_ep: src: replace SEGGER RTT with UART console output
    Replace APP_PRINT/APP_ERR_PRINT/APP_ERR_TRAP macros to use console_print
    instead of SEGGER_RTT_printf. Remove APP_READ and APP_CHECK_DATA macros.
    Add console_init() call in sender_task_entry before banner print.

--- kakip_rtos_example/freertos_kakip_cm33_ep/e2studio/src/common_utils.h
+++ kakip_rtos_example/freertos_kakip_cm33_ep/e2studio/src/common_utils.h
@@ -20,8 +20,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include "hal_data.h"
-/* SEGGER RTT and error related headers */
-#include "SEGGER_RTT.h"
+/* UART console output */
+#include "console.h"
 
 #define BIT_SHIFT_8  (8u)
 #define SIZE_64      (64u)
@@ -39,18 +39,13 @@
                                 "\r\nRefer to readme.txt file for more details on Example Project and" \
                                 "\r\nFSP User's Manual for more information about "MODULE_NAME" driver\r\n"
 
-#define SEGGER_INDEX            (0)
-#define APP_PRINT(fn_, ...)      (SEGGER_RTT_printf (SEGGER_INDEX,(fn_), ##__VA_ARGS__))
+#define APP_PRINT(fn_, ...)      console_print((fn_), ##__VA_ARGS__)
 
 #define APP_ERR_PRINT(fn_, ...)  if(LVL_ERR)\
-        SEGGER_RTT_printf (SEGGER_INDEX, "[ERR] In Function: %s(), %s",__FUNCTION__,(fn_),##__VA_ARGS__);
+        console_print("[ERR] In Function: %s(), %s",__FUNCTION__,(fn_),##__VA_ARGS__);
 
 #define APP_ERR_TRAP(err)        if(err) {\
-        SEGGER_RTT_printf(SEGGER_INDEX, "\r\nReturned Error Code: 0x%x  \r\n", err);\
+        console_print("\r\nReturned Error Code: 0x%x  \r\n", err);\
         __asm("BKPT #0\n");} /* trap upon the error  */
-
-#define APP_READ(read_data)     SEGGER_RTT_Read (SEGGER_INDEX, read_data, sizeof(read_data));
-
-#define APP_CHECK_DATA          SEGGER_RTT_HasKey()
 
 #endif /* COMMON_UTILS_H_ */
